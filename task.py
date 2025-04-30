77a95c3b492101869a3d88f2699828cbf9bcbe22
Veer1578 <sweetyatulshah@gmail.com>
2025-04-30 09:32:16 +0530

    task

---
+++ task.py
@@ -0,0 +1,15 @@
+def square(a):
+    return 4 * a
+
+
+side = int(input('Enter the side of a square '))
+print(f'The perimeter of that square is {square(side)}')
+
+
+def rectangle(l, b):
+    return 2 * (l + b)
+
+
+length = int(input('Enter the length of a rectangle '))
+breadth = int(input('Enter the breadth of a rectangle '))
+print(f'The perimeter of that rectangle is {rectangle(length, breadth)}')
